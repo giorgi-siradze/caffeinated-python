cf988da040f48a8b905ed391960f331e5a1d9213
Giorgi Siradze <96883138+giorgi-siradze@users.noreply.github.com>
2024-12-26 21:16:56 +0400

    refactor: remove main() from MinimalInterpreter.java, add to Main.java instead

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test");
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+
+        // Example program: Calculate and print the sum of 10 and 20
+        String program = """
+           x=5
+           y=6
+           print(x+y)
+        """;
+
+        interpreter.eval(program);
     }
 }
--- MinimalInterpreter.java
+++ MinimalInterpreter.java
@@ -177,18 +177,6 @@ public class MinimalInterpreter {
     }
 
 
-    public static void main(String[] args) {
-        MinimalInterpreter interpreter = new MinimalInterpreter();
-
-        // Example program: Calculate and print the sum of 10 and 20
-        String program = """
-           x=5
-           y=6
-           print(x+y)
-        """;
-
-        interpreter.eval(program);
-    }
      // checks if expression contains any string variables
     private boolean containsStringVariable(String expression) {
         String[] stringParts = expression.split("[+\\-,]");
