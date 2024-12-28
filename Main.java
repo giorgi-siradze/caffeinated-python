d2f9acc23cfdf61581896af32543d48a3762cd66
Tatia Tkeshelashvili <tyeshelashvilitatia@gmail.com>
2024-12-28 23:25:06 +0400

    added sumofdigits nthfibonacci and multtableoffive algorithms

--- Main.java
+++ Main.java
@@ -1,15 +1,67 @@
 public class Main {
     public static void main(String[] args) {
         String code = """
-                n = 5
-                i = 1
-                while n >=1:
-                          i = i * n
-                          n = n - 1
-                print(i)
+                N = 10
+                first = 0
+                second = 1
+                count = 1
+                while count < N:
+                    first = first + second 
+                    second = first - second  
+                    count = count + 1
+                print(first)
                 """;
 
         MinimalInterpreter interpreter2 = new MinimalInterpreter();
         interpreter2.eval(code);
     }
+    public static void sumOfDigitsTest()
+    {
+        String code = """
+                number = 1234
+                total = 0
+                digit = 0
+                while number > 0:
+                    digit = number % 10 # 4 -> 3 -> 2 -> 1
+                    total += digit
+                    number = number - digit
+                    number = number / 10
+                print(total)
+                """;
+
+        MinimalInterpreter interpreter2 = new MinimalInterpreter();
+        interpreter2.eval(code);
+    }
+    public static void multTableOfFive()
+    {
+        String code = """
+                n = 1
+                p = 5
+                while n <= 10:
+                    print(p * n)
+                    n = n + 1
+                """;
+
+        MinimalInterpreter interpreter2 = new MinimalInterpreter();
+        interpreter2.eval(code);
+    }
+
+    public static void NthFibonacci()
+    {
+        String code = """
+                N = 10
+                first = 0
+                second = 1
+                count = 1
+                while count < N:
+                    first = first + second 
+                    second = first - second  
+                    count = count + 1
+                print(first)
+                """;
+
+        MinimalInterpreter interpreter2 = new MinimalInterpreter();
+        interpreter2.eval(code);
+    }
+
 }
