13797f734b3b3f08c1ae9e0544e49ff3cc051d06
Giorgi Siradze <96883138+giorgi-siradze@users.noreply.github.com>
2024-12-29 21:21:05 +0400

    algorithms: add task 1, 2. testing: every task works except 5, 6, 7

--- Main.java
+++ Main.java
@@ -1,20 +1,59 @@
 public class Main {
     public static void main(String[] args) {
       // This method is for main code testing, NOT for algorithms.
-//      String code = """
-//                """;
-//
-//        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-//        interpreter2.eval(code);
+      String code = """
+                a = 10
+                b = 10
+                print(a == b)
+                """;
+
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
 
         // Algorithms should be called below.
-        gcd(); // Works as intended
-        reverseAnumber(); // Works as intended
-        sumOfDigits(); // Works as intended
-        multiplicationTable(); // Works as intended
-        NthFibonacci(); // Works as intended
+//        sumOfNNumbers(); // Works as intended
+//        factorial(); // Works as intended
+//        gcd(); // Works as intended
+//        reverseNumber(); // Works as intended, but returns Double
+//        isPrime(); // Doesn't work as intended, needs revision
+//        isPalindrome(); // Doesn't work as intended, needs revision
+//        largestDigit(); // Doesn't work as intended, needs revision
+//        sumOfDigits(); // Works as intended
+//        multiplicationTable(); // Works as intended
+//        NthFibonacci(); // Works as intended
     }
 
+    // Task 1
+    // Contributor: Giorgi Siradze
+    public static void sumOfNNumbers() {
+        String code = """
+                n = 10
+                sum = 0
+                while n != 0:
+                    sum = sum + n
+                    n = n - 1.0
+                print(sum)
+                """;
+
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
+    }
+
+    // Task 2
+    // Contributor: Giorgi Siradze
+    public static void factorial() {
+        String code = """
+            n = 5
+            result = 1
+            while n > 1:
+                result = result * n
+                n = n - 1
+            print(result)
+            """;
+
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
+    }
 
     // Task 3
     // Contributor: Ana Narmania
@@ -29,13 +68,13 @@ public class Main {
                 print(a)
                 """;
 
-        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-        interpreter2.eval(code);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
     // Task 4
     // Contributor: Ana Narmania
-    public static void reverseAnumber() {
+    public static void reverseNumber() {
         String code = """
                 a = 1234
                 reversed_a = 0
@@ -45,10 +84,10 @@ public class Main {
                       a = a / 10
                       reversed_a = reversed_a * 10 + remainder
                 print(reversed_a)
-        """;
+                """;
 
-        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-        interpreter2.eval(code);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
 
@@ -56,7 +95,7 @@ public class Main {
     // Contributor: Nini Phkhakadze
     public static void isPrime() {
         String code = """
-            n = 13
+            n = 14
             is_prime = 1
             i = 2
             while i * i <= n:
@@ -64,45 +103,100 @@ public class Main {
                 i = i + 1
             is_prime = is_prime * (n > 1)
             print(is_prime == 1)
-        """;
+            """;
+//
+//        String code = """
+//            n = 13
+//            prime = True
+//
+//            if n < 2:
+//                prime = False
+//
+//            i = 2
+//            while i * i <= n:
+//                if n % i == 0:
+//                    prime = False
+//                i = i + 1
+//
+//            print(prime)
+//            """;
 
-        MinimalInterpreter interpreter2;
-        interpreter2.eval(code);
+//        String code = """
+//                n = 19
+//                prime = True
+//
+//                if n < 2:
+//                    prime = False
+//
+//                i = 2
+//                while prime and i < n:
+//                    if n % i == 0:
+//                        prime = False
+//                    i = i + 1
+//
+//                print(prime)
+//                """;
+
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
     // Task 6
     // Contributor: Nini Phkhakadze
     public static void isPalindrome() {
-        String code = """
-                n = 121
-                original = n
-                reversed_n = 0
-                while n != 0:
-                    remainder = n % 10
-                    reversed_n = reversed_n * 10 + remainder
-                    n = n // 10
-                print(original == reversed_n)
-        """;
+//        String code = """
+//                n = 121
+//                original = n
+//                reversed_n = 0
+//                while n > 0:
+//                    remainder = n % 10
+//                    reversed_n = reversed_n * 10 + remainder
+//                    n = n - remainder
+//                    n = n / 10
+//                print(original == reversed_n)
+//                """;
 
-        MinimalInterpreter interpreter2;
-        interpreter2.eval(code);
+//        String code = """
+//                n = 121
+//                original = n
+//                reversed = 0
+//                digit = 0
+//
+//                while n > 0:
+//                    digit = n % 10
+//                    reversed = reversed * 10 + digit
+//                    n = n - digit
+//                    n = n / 10
+//                print(original == reversed)
+//                """;
+
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+//        interpreter.eval(code);
     }
 
     // Task 7
     // Contributor: Nini Phkhakadze
     public static void largestDigit() {
-        String codeString = """
-            n = 3947
+        String code = """
+            n = 3247
             largest_digit = 0
+            largest_more_digit = 0
+            digit_more_largest = 0
             while n != 0:
                 digit = n % 10
-                largest_digit = largest_digit * (largest_digit > digit) + digit * (digit > largest_digit)
-                n = n // 10
+                
+                largest_more_digit = largest_digit > digit
+                print(largest_digit > digit)
+                print(largest_more_digit)
+                digit_more_largest = digit > largest_digit
+                print(digit_more_largest)
+                largest_digit = largest_digit * largest_more_digit + digit * digit_more_largest
+                n = n / 10
             print(largest_digit)
-        """;
+            """;
 
-        MinimalInterpreter interpreter2;
-        interpreter2.eval(codeString);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
     // Task 8
@@ -118,10 +212,10 @@ public class Main {
                     number = number - digit
                     number = number / 10
                 print(total)
-        """;
+                """;
 
-        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-        interpreter2.eval(code);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
     // Task 9
@@ -133,10 +227,10 @@ public class Main {
                 while n <= 10:
                     print(p * n)
                     n = n + 1
-        """;
+                """;
 
-        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-        interpreter2.eval(code);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 
     // Task 10
@@ -152,9 +246,9 @@ public class Main {
                     second = first - second
                     count = count + 1
                 print(first)
-        """;
+                """;
 
-        MinimalInterpreter interpreter2 = new MinimalInterpreter();
-        interpreter2.eval(code);
+        MinimalInterpreter interpreter = new MinimalInterpreter();
+        interpreter.eval(code);
     }
 }
